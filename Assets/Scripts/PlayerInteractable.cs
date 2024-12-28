90e677b43485a5176f8d9efdc6f158013bb7d631
Ayşe Elif Çelik <celik.ayse@std.izu.edu.tr>
2024-12-28 15:39:04 +0300

    notificationList RPC fixed

--- Assets/Scripts/PlayerInteractable.cs
+++ Assets/Scripts/PlayerInteractable.cs
@@ -7,47 +7,65 @@ using Photon.Realtime;
 public class PlayerInteractable : MonoBehaviour
 {
     private void OnMouseDown()
-{
-    Photon.Realtime.Player clickedPlayer = GetComponent<PhotonView>().Owner;
-
-    if (clickedPlayer != null)
     {
-        string playerName = clickedPlayer.NickName;
-        Debug.Log($"Tıklanan oyuncu: {playerName}");
+        UpdateNotificationList();
+    }
 
-        // Ebe olan oyuncunun kendisine tıklanması durumunda işlemi engelle
-        if (PhotonNetwork.LocalPlayer.CustomProperties["Role"]?.ToString() == "EBE")
+    [PunRPC]
+    public void UpdateNotificationList()
+    {
+        Photon.Realtime.Player clickedPlayer = GetComponent<PhotonView>().Owner;
+
+        if (clickedPlayer != null)
         {
-            if (clickedPlayer == PhotonNetwork.LocalPlayer)
-            {
-                Debug.Log("Ebe kendisine tıklayamaz!");
-                return;  // Eğer tıklanan oyuncu Ebe ve kendisi ise, işlemi sonlandır
-            }
+            string playerName = clickedPlayer.NickName;
+            Debug.Log($"Tıklanan oyuncu: {playerName}");
 
-            Debug.Log($"{playerName} isimli oyuncuya tıkladınız!");
-
-            // Eğer tıklanan oyuncu sobelenemezler listesinde değilse ve seenPlayers listesinde değilse
-            if (!GameManager.Instance.sobelenemezler.Contains(clickedPlayer) &&
-                !GameManager.Instance.seenPlayers.Contains(clickedPlayer))
+            // Ebe olan oyuncunun kendisine tıklanması durumunda işlemi engelle
+            if (PhotonNetwork.LocalPlayer.CustomProperties["Role"]?.ToString() == "EBE")
             {
-                // Hiding rolünde olduğu kontrolü
-                if (GameManager.Instance.GetPlayerRole(clickedPlayer) == "Hiding")
+                if (clickedPlayer == PhotonNetwork.LocalPlayer)
                 {
-                    GameManager.Instance.seenPlayers.Add(clickedPlayer);
-                    Debug.Log($"{playerName} GameManager'daki seenPlayers listesine eklendi!");
-                    GameManager.Instance.notificationList.Add($"{playerName} is seen!!!");
+                    Debug.Log("Ebe kendisine tıklayamaz!");
+                    return;  // Eğer tıklanan oyuncu Ebe ve kendisi ise, işlemi sonlandır
+                }
+
+                Debug.Log($"{playerName} isimli oyuncuya tıkladınız!");
+
+                // Eğer tıklanan oyuncu sobelenemezler listesinde değilse ve seenPlayers listesinde değilse
+                if (!GameManager.Instance.sobelenemezler.Contains(clickedPlayer) &&
+                    !GameManager.Instance.seenPlayers.Contains(clickedPlayer))
+                {
+                    // Hiding rolünde olduğu kontrolü
+                    if (GameManager.Instance.GetPlayerRole(clickedPlayer) == "Hiding")
+                    {
+                        GameManager.Instance.seenPlayers.Add(clickedPlayer);
+                        Debug.Log($"{playerName} GameManager'daki seenPlayers listesine eklendi!");
+
+                        // Bu bildirimi tüm oyunculara göndereceğiz
+                        PhotonView photonView = PhotonView.Get(this);
+                        photonView.RPC("ShowNotificationOnAllClients", RpcTarget.All, $"{playerName} is seen!!!");
+                    }
+                }
+                else
+                {
+                    Debug.Log($"{playerName} sobelenemezler listesinde veya zaten seenPlayers listesinde!");
                 }
             }
-            else
-            {
-                Debug.Log($"{playerName} sobelenemezler listesinde veya zaten seenPlayers listesinde!");
-            }
+        }
+        else
+        {
+            Debug.Log("Tıklanan nesnede Photon.Realtime.Player scripti yok.");
         }
     }
-    else
-    {
-        Debug.Log("Tıklanan nesnede Photon.Realtime.Player scripti yok.");
-    }
-}
 
+    // Bu RPC fonksiyonu tüm oyuncularda çalışacak ve bildirimi gösterecek
+    [PunRPC]
+    public void ShowNotificationOnAllClients(string notificationMessage)
+    {
+        Debug.Log($"Bildirim Gönderildi: {notificationMessage}");
+
+        // Bildirimi tüm oyunculara göster (örneğin UI ile)
+        GameManager.Instance.notificationList.Add(notificationMessage);
+    }
 }
